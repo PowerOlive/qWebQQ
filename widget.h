eb7bca504324264c00d73b8fb390b241c7e228a9
c0debreak <the.warl0ck.1989@gmail.com>
2012-05-06 09:40:02 +0800

    Relicense

--- widget.h
+++ widget.h
@@ -1,3 +1,29 @@
+/* BEGIN_COMMON_COPYRIGHT_HEADER
+ * (c)LGPL2+
+ *
+ * Razor - a lightweight, Qt based, desktop toolset
+ * http://razor-qt.org
+ *
+ * Copyright: 2012 Razor team
+ * Authors:
+ *   Aaron Lewis <the.warl0ck.1989@gmail.com>
+ *
+ * This program or library is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA
+ *
+ * END_COMMON_COPYRIGHT_HEADER */
 #ifndef WIDGET_H
 #define WIDGET_H
 
@@ -23,7 +49,7 @@ public:
     explicit Widget(QWidget *parent = 0);
     ~Widget();
 
-    void msgbox (const QString & msg , const QString & title = QString ("错误") );
+    void msgbox (const QString & msg , const QString & title = QString::fromUtf8 ("错误") );
     bool question (const char* msg)
     {
         return question (QString::fromUtf8(msg));
